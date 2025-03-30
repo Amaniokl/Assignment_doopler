02be04b128a918beec682af83bfeb37c0c6b984f
Amaniokl <amanoutlook2003@gmail.com>
2025-03-31 00:41:34 +0530

    minor improvisation

--- src/server.js
+++ src/server.js
@@ -12,6 +12,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send('Server is live!');
+  });
 // Routes
 app.use("/api", imageRoutes);
 
